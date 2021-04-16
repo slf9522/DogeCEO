3ff0bbf64b3301a2b8ec91fe92a15a388d451e9e
fangjiaxin <fangjiaxin@meituan.com>
2021-04-16 15:06:44 +0800

    test

--- TweetBot.py
+++ TweetBot.py
@@ -1,6 +1,9 @@
 import requests
 import json
 from dotenv import load_dotenv
+
+from notify import Notify
+
 load_dotenv()
 import os
 import time
@@ -12,23 +15,27 @@ from difflib import SequenceMatcher
 
 
 # ger twilio account sid
-def sid(): 
+def sid():
     return os.getenv("TWILIO_ACCOUNT_SID")
 
-#get twilio auth token
+
+# get twilio auth token
 def twilioAuth():
     return os.getenv("TWILIO_AUTH_TOKEN")
 
-#get twitter bearer token
+
+# get twitter bearer token
 def auth():
     return os.getenv("BEARER_TOKEN")
 
-# funtion that returns witter timeline url 
+
+# funtion that returns witter timeline url
 def create_url():
-    user_id = 1358539990670536705 # elon: 44196397 # test account: 1358539990670536705
+    user_id = 1358539990670536705  # elon: 44196397 # test account: 1358539990670536705
     return "https://api.twitter.com/2/users/{}/tweets".format(user_id)
 
-#returns tweet parameters for twitter api 
+
+# returns tweet parameters for twitter api
 def get_params():
     # Tweet fields are adjustable.
     # Options include:
@@ -39,10 +46,12 @@ def get_params():
     # source, text, and withheld
     return {"tweet.fields": "created_at,attachments"}
 
-#authenticate bearer token 
+
+# authenticate bearer token
 def create_headers(bearer_token):
     headers = {"Authorization": "Bearer {}".format(bearer_token)}
     return headers
+
 
 # connect to twitter api and return json
 def connect_to_endpoint(url, headers, params):
@@ -56,103 +65,115 @@ def connect_to_endpoint(url, headers, params):
         )
     return response.json()
 
+
 # function that return similarity index of 2 strings
 def similar(a, b):
     return SequenceMatcher(None, a, b).ratio()
 
+
 def main():
+    notify = Notify()
+
     lastDate = datetime.strptime('2021-01-01 20:05:25.000', '%Y-%m-%d %H:%M:%S.%f')
 
-    #twilio setup
-    account_sid = sid()
-    auth_token = twilioAuth()
-    client = Client(account_sid, auth_token)
-    phone_number = os.getenv("PHONE_NUMBER")
-    twilio_phone_number = os.getenv("TWILIO_PHONE_NUMBER")
-
-
-    #twitter setup
+    # # twilio setup
+    # account_sid = sid()
+    # auth_token = twilioAuth()
+    # client = Client(account_sid, auth_token)
+    # phone_number = os.getenv("PHONE_NUMBER")
+    # twilio_phone_number = os.getenv("TWILIO_PHONE_NUMBER")
+    #
+    # twitter setup
     bearer_token = auth()
     url = create_url()
     headers = create_headers(bearer_token)
     params = get_params()
 
-    #ImageAi setup
+    # ImageAi setup
     current_directory = os.getcwd()
     detector = ObjectDetection()
     detector.setModelTypeAsYOLOv3()
-    detector.setModelPath(os.path.join(current_directory, "yolo.h5"))
+    detector.setModelPath(os.path.join(current_directory, "yolo-tiny.h5"))
     detector.loadModel()
 
     time.sleep(5)
 
-    #constant loop that runs every 2 minutes
+    # constant loop that runs every 2 minutes
     while True:
-        json_response = connect_to_endpoint(url, headers, params) #get latest tweets
-        #print(json.dumps(json_response, indent=4, sort_keys=True))
+        json_response = connect_to_endpoint(url, headers, params)  # get latest tweets
+        # print(json.dumps(json_response, indent=4, sort_keys=True))
         tweets = json_response['data']
-        #print(tweets)
+        # print(tweets)
         print("Search: ")
         for tweet in tweets:
-            tweetDate = datetime.strptime(tweet['created_at'][:-1], '%Y-%m-%dT%H:%M:%S.%f') #get the time the tweet was posted
-            if tweetDate > lastDate: # check if tweet has already been analyzed based on time of last tweet analyzed
-                tweetContent = tweet['text'].lower() #format all letters to lowercase 
-                splitTweetContent = tweetContent.split() #split tweet into words 
+            tweetDate = datetime.strptime(tweet['created_at'][:-1],
+                                          '%Y-%m-%dT%H:%M:%S.%f')  # get the time the tweet was posted
+            if tweetDate > lastDate:  # check if tweet has already been analyzed based on time of last tweet analyzed
+                tweetContent = tweet['text'].lower()  # format all letters to lowercase
+                splitTweetContent = tweetContent.split()  # split tweet into words
 
-
-                with open ('cryptos.json') as cryptoJsonFile: 
-                    cryptoData = json.load(cryptoJsonFile) #get cryptodata from cryptos.json file
-                    for crypto in cryptoData['cryptos']: #iterate through each cryto 
-                        for word in splitTweetContent: #iterate through each word in the tweet
-                            if similar(word, crypto['symbol'].lower() ) > 0.75 or similar(word, crypto['name'].lower()) > 0.65 : #check if word is similar to crypto symbol or name
-                                cryptoMessage = "crypto found: " + crypto['name'] + " in Elon's tweet: " + tweet['text'] #generate crypto found message
+                with open('cryptos.json') as cryptoJsonFile:
+                    cryptoData = json.load(cryptoJsonFile)  # get cryptodata from cryptos.json file
+                    for crypto in cryptoData['cryptos']:  # iterate through each cryto
+                        for word in splitTweetContent:  # iterate through each word in the tweet
+                            if similar(word, crypto['symbol'].lower()) > 0.75 or similar(word, crypto[
+                                'name'].lower()) > 0.65:  # check if word is similar to crypto symbol or name
+                                cryptoMessage = "crypto found: " + crypto['name'] + " in Elon's tweet: " + tweet[
+                                    'text']  # generate crypto found message
                                 print(cryptoMessage)
-                               #send SMS with Twilio
-                                client.messages.create(
-                                    body=cryptoMessage,
-                                    from_=twilio_phone_number,
-                                    to=phone_number
-                                )
+                                # #send SMS with Twilio
+                                #  client.messages.create(
+                                #      body=cryptoMessage,
+                                #      from_=twilio_phone_number,
+                                #      to=phone_number
+                                #  )
+                                notify.tgBot(cryptoMessage)
                                 print("\n")
 
                 with open('stocks.json') as stocksJsonFile:
-                    stockData = json.load(stocksJsonFile) #get stockData from stocks.json file
-                    for stock in stockData['popular_stocks']: #iterate through each stock
-                        for word in splitTweetContent: #iterate through each word in the tweet
-                            if similar(word, stock['name'].lower()) > 0.75: #check if word is similar to stock name
-                                stockMessage = "stock found: " + stock['name'] + " in Elon's tweet: " + tweet['text'] #generate stock found message
+                    stockData = json.load(stocksJsonFile)  # get stockData from stocks.json file
+                    for stock in stockData['popular_stocks']:  # iterate through each stock
+                        for word in splitTweetContent:  # iterate through each word in the tweet
+                            if similar(word, stock['name'].lower()) > 0.75:  # check if word is similar to stock name
+                                stockMessage = "stock found: " + stock['name'] + " in Elon's tweet: " + tweet[
+                                    'text']  # generate stock found message
                                 print(stockMessage)
-                                #send SMS with Twilio
-                                client.messages.create(
-                                    body=cryptoMessage,
-                                    from_=twilio_phone_number,
-                                    to=phone_number
-                                )
+                                # #send SMS with Twilio
+                                # client.messages.create(
+                                #     body=cryptoMessage,
+                                #     from_=twilio_phone_number,
+                                #     to=phone_number
+                                # )
+                                notify.tgBot(cryptoMessage)
                                 print("\n")
-                               
+
                 # check tweets for images
                 try:
-                    if "attachments" in tweet: # if tweet potentially contains images
-                        requestURL = "https://api.twitter.com/2/tweets?ids=" + tweet['id'] +"&expansions=attachments.media_keys&media.fields=duration_ms,height,media_key,preview_image_url,public_metrics,type,url,width" #send request to get tweet by tweet ID
-                        requestTweet = requests.get(requestURL, headers={"Authorization": "Bearer {}".format(bearer_token)})
-                        for media in requestTweet.json()['includes']['media']: #iterate through media array of tweet
+                    if "attachments" in tweet:  # if tweet potentially contains images
+                        requestURL = "https://api.twitter.com/2/tweets?ids=" + tweet[
+                            'id'] + "&expansions=attachments.media_keys&media.fields=duration_ms,height,media_key,preview_image_url,public_metrics,type,url,width"  # send request to get tweet by tweet ID
+                        requestTweet = requests.get(requestURL,
+                                                    headers={"Authorization": "Bearer {}".format(bearer_token)})
+                        for media in requestTweet.json()['includes']['media']:  # iterate through media array of tweet
                             imageURL = media['url']
-                            urllib.request.urlretrieve(imageURL,"tweeted-image.jpg") #retrieve and download image from media
+                            urllib.request.urlretrieve(imageURL,
+                                                       "tweeted-image.jpg")  # retrieve and download image from media
                             time.sleep(5)
-                            #use imageAI to detect objects in image
+                            # use imageAI to detect objects in image
                             detections = detector.detectObjectsFromImage(
                                 input_image=os.path.join(current_directory, "tweeted-image.jpg"),
                                 output_image_path=os.path.join(current_directory, "labeled-tweeted-image.jpg"))
-                            for eachObject in detections: #iterate through each object in image
-                                if eachObject["name"] == "dog": #check if object is dog
+                            for eachObject in detections:  # iterate through each object in image
+                                if eachObject["name"] == "dog":  # check if object is dog
                                     dogeMessage = "found dog in image: " + imageURL
                                     print(dogeMessage)
-                                    #send SMS with Twilio
-                                    client.messages.create(
-                                    body=cryptoMessage,
-                                    from_=twilio_phone_number,
-                                    to=phone_number
-                                    )
+                                    notify.tgBot(cryptoMessage)
+                                    # #send SMS with Twilio
+                                    # client.messages.create(
+                                    # body=cryptoMessage,
+                                    # from_=twilio_phone_number,
+                                    # to=phone_number
+                                    # )
                 except:
                     print("error")
 
@@ -161,9 +182,6 @@ def main():
         time.sleep(60)
         print("Waiting 60 seconds")
         time.sleep(60)
-
-
-
 
 
 if __name__ == "__main__":
